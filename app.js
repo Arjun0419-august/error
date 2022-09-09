b6a836058a27c7339c32ef7c47ff5178471f4648
Arjun Rai <raiarjun0419@gmail.com>
2022-09-09 18:33:34 +0545

    message

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+ const app = express()
+
+const logger = require('./logger')
+const authorize = require('./authorize')
+
+app.use([authorize,logger])
+
+app.get('/', (req,res) => {
+    
+    res.send('Home page')
+})
+
+app.get('/about', (req,res) => {
+    res.send('About page')
+})
+app.get('/api/products', (req,res) => {
+    
+    res.send('Products')
+})
+
+app.get('/api/items', (req,res) => {
+    res.send('Items')
+})
+
+
+ app.listen(5000,() => {
+    console.log('Server is listening on port 5000....');
+ })
